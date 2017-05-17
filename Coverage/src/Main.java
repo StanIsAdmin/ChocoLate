ae3386aca53bf899c0a5578ba826f906cf75703a
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-05-17 21:16:06 +0200

    Create Parser class, separate from Main

--- Coverage/src/Main.java
+++ Coverage/src/Main.java
@@ -1,15 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.OptionGroup;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import parser.Parser;
 import pieces.Bishop;
 import pieces.Knight;
 import pieces.Rook;
@@ -19,149 +8,49 @@ import problems.IndependenceProblem;
 
 
 public class Main {
-    // CLI argument parsing
-    private static Options options = new Options();
-    private static CommandLine cmd;
-    private static CommandLineParser parser = new DefaultParser();
-    private static HelpFormatter formatter = new HelpFormatter();
-    
-    // parsed arguments
-    static boolean independence, domination;
-    static String independenceOpt="i", dominationOpt="d";
-    static boolean minimize;
-    static String minimizeOpt="m";
-    static int boardSize, rooks, bishops, knights;
-    static String boardSizeOpt="n", rooksOpt="t", bishopsOpt="f", knightsOpt="c";
-    
-    static class OptionComparator<T extends Option> implements Comparator<T> {
-        private final List<String> OPTS_ORDER; //order of options
-        
-        public OptionComparator(List<String> optionsOrder) {
-            OPTS_ORDER = optionsOrder;
-        }
-        
-        @Override
-        public int compare(T o1, T o2) {
-            return OPTS_ORDER.indexOf(o1.getOpt()) - OPTS_ORDER.indexOf(o2.getOpt());
-        }
-    }
+    /* command-line arguments parser */
+    private static Parser _parser;
     
     /**
     * @param args the command line arguments
     */
     public static void main(String[] args) {
-        initOptions();
-        parseArgs(args);
+        _parser = new Parser();
+        _parser.parse(args);
         start();
-    }
-    
-    private static void initOptions() {
-        // type of problem
-        options.addOption(dominationOpt, false, "solve a domination problem");
-        options.addOption(independenceOpt, false, "solve an independence problem");
-        OptionGroup problemType = new OptionGroup();
-        problemType.addOption(options.getOption(dominationOpt));
-        problemType.addOption(options.getOption(independenceOpt));
-        problemType.setRequired(true);
-        options.addOptionGroup(problemType);
-        
-        // objective
-        options.addOption(minimizeOpt, "minimize", false,
-                "minimize the total number of pieces\n"
-                + " - numbers of pieces are used as upper bounds"
-        );
-        
-        // board parameters
-        addPieceOption(boardSizeOpt, "size", "size of the board (default is 8)");
-        addPieceOption(rooksOpt, "rooks", "number of rooks (default is 0)");
-        addPieceOption(bishopsOpt, "bishops", "number of bishops (default is 0)");
-        addPieceOption(knightsOpt, "knights", "number of knights (default is 0)");
-        
-        // order of displayed arguments
-        List<String> optionsOrder = Arrays.asList(
-                dominationOpt, independenceOpt, boardSizeOpt,
-                rooksOpt, bishopsOpt, knightsOpt, minimizeOpt
-        );
-        formatter.setOptionComparator(new OptionComparator(optionsOrder));
-    }
-    
-    private static void addPieceOption(String optionName, String argName, String description) {
-        options.addOption(
-                OptionBuilder.withArgName(argName).hasArg(true).withDescription(description)
-                        .withType(Integer.class).create(optionName)
-        );
-    }
-    
-    private static void parseArgs(String[] args) {
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            quit();
-        }
-        
-        // type of problem
-        independence = cmd.hasOption(independenceOpt);
-        domination = cmd.hasOption(dominationOpt);
-        
-        // objective
-        minimize = cmd.hasOption(minimizeOpt);
-        
-        // board parameters
-        boardSize = getPieceOption(boardSizeOpt, 8);
-        rooks = getPieceOption(rooksOpt, 0);
-        bishops = getPieceOption(bishopsOpt, 0);
-        knights = getPieceOption(knightsOpt, 0);
-    }
-        
-    private static int getPieceOption(String optionName, int defaultValue) {
-        int result = defaultValue;
-        if (cmd.hasOption(optionName)) {
-            try {
-                result = Integer.parseInt(cmd.getOptionValue(optionName));
-            } catch (NumberFormatException ex) {
-                System.out.println(ex);
-                quit();
-            }
-        }
-        return result;
     }
     
     private static void start() {
         AbstractCoverageProblem prob;
-        if (independence) {
-            prob = new IndependenceProblem(boardSize);
-            solveProblem(prob);
-        }
-        if (domination) {
+        int boardSize = _parser.getBoardSize();
+        
+        if (_parser.problemIsDomination()) {
             prob = new DominationProblem(boardSize);
-            solveProblem(prob);
+        } else {
+            prob = new IndependenceProblem(boardSize);
         }
+            
+        solveProblem(prob);
     }
     
     private static void solveProblem(AbstractCoverageProblem prob) {
         // add required pieces
-        for (int i=0; i<rooks; i++) {
+        for (int i=0; i<_parser.getRookCount(); i++) {
             prob.addPiece(new Rook());
         }
-        for (int i=0; i<bishops; i++) {
+        for (int i=0; i<_parser.getBishopCount(); i++) {
             prob.addPiece(new Bishop());
         }
-        for (int i=0; i<knights; i++) {
+        for (int i=0; i<_parser.getKnightCount(); i++) {
             prob.addPiece(new Knight());
         }
         
         //Solve with objective
-        if (minimize) {
+        if (_parser.objectiveIsMinimize()) {
             prob.solveMinimum();
         } else {
             prob.solve();
         }
         System.out.print(prob.getSolutionAsString().replace("-", "*"));
-    }
-    
-    private static void quit() {
-        formatter.printHelp("board solver", options);
-        System.exit(1);
     }
 }
---
+++ Coverage/src/parser/Parser.java
@@ -0,0 +1,151 @@
+package parser;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * Parses the command-line arguments.
+ */
+public class Parser {
+    // CLI argument parsing
+    private static final Options options = new Options();
+    private static final CommandLineParser parser = new DefaultParser();
+    private static final HelpFormatter formatter = new HelpFormatter();
+    private CommandLine cmd;
+    
+    // parsed arguments
+    static boolean independence, domination;
+    static String independenceOpt="i", dominationOpt="d";
+    static boolean minimize;
+    static String minimizeOpt="m";
+    static int boardSize, rooks, bishops, knights;
+    static String boardSizeOpt="n", rooksOpt="t", bishopsOpt="f", knightsOpt="c";
+    
+    static class OptionComparator<T extends Option> implements Comparator<T> {
+        private final List<String> OPTS_ORDER; //order of options
+        
+        public OptionComparator(List<String> optionsOrder) {
+            OPTS_ORDER = optionsOrder;
+        }
+        
+        @Override
+        public int compare(T o1, T o2) {
+            return OPTS_ORDER.indexOf(o1.getOpt()) - OPTS_ORDER.indexOf(o2.getOpt());
+        }
+    }
+    
+    public Parser() {
+        // type of problem
+        options.addOption(dominationOpt, false, "solve a domination problem");
+        options.addOption(independenceOpt, false, "solve an independence problem");
+        OptionGroup problemType = new OptionGroup();
+        problemType.addOption(options.getOption(dominationOpt));
+        problemType.addOption(options.getOption(independenceOpt));
+        problemType.setRequired(true);
+        options.addOptionGroup(problemType);
+        
+        // objective
+        options.addOption(minimizeOpt, "minimize", false,
+                "minimize the total number of pieces\n"
+                + " - numbers of pieces are used as upper bounds"
+        );
+        
+        // board parameters
+        addPieceOption(boardSizeOpt, "size", "size of the board (default is 8)");
+        addPieceOption(rooksOpt, "rooks", "number of rooks (default is 0)");
+        addPieceOption(bishopsOpt, "bishops", "number of bishops (default is 0)");
+        addPieceOption(knightsOpt, "knights", "number of knights (default is 0)");
+        
+        // order of displayed arguments
+        List<String> optionsOrder = Arrays.asList(
+                dominationOpt, independenceOpt, boardSizeOpt,
+                rooksOpt, bishopsOpt, knightsOpt, minimizeOpt
+        );
+        formatter.setOptionComparator(new OptionComparator(optionsOrder));
+    }
+    
+    private static void addPieceOption(String optionName, String argName, String description) {
+        options.addOption(
+                OptionBuilder.withArgName(argName).hasArg(true).withDescription(description)
+                        .withType(Integer.class).create(optionName)
+        );
+    }
+    
+    public void parse(String[] args) {
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            quit();
+        }
+        
+        // type of problem
+        independence = cmd.hasOption(independenceOpt);
+        domination = cmd.hasOption(dominationOpt);
+        
+        // objective
+        minimize = cmd.hasOption(minimizeOpt);
+        
+        // board parameters
+        boardSize = getPieceOption(boardSizeOpt, 8);
+        rooks = getPieceOption(rooksOpt, 0);
+        bishops = getPieceOption(bishopsOpt, 0);
+        knights = getPieceOption(knightsOpt, 0);
+    }
+        
+    private int getPieceOption(String optionName, int defaultValue) {
+        int result = defaultValue;
+        if (cmd.hasOption(optionName)) {
+            try {
+                result = Integer.parseInt(cmd.getOptionValue(optionName));
+            } catch (NumberFormatException ex) {
+                System.err.println(ex);
+                quit();
+            }
+        }
+        return result;
+    }
+    
+    private void quit() {
+        formatter.printHelp("board solver", options);
+        System.exit(1);
+    }
+    
+    public boolean problemIsDomination() {
+        return domination;
+    }
+    
+    public boolean problemIsIndependence() {
+        return independence;
+    }
+    
+    public boolean objectiveIsMinimize() {
+        return minimize;
+    }
+    
+    public int getBoardSize() {
+        return boardSize;
+    }
+    
+    public int getRookCount() {
+        return rooks;
+    }
+    
+    public int getBishopCount() {
+        return bishops;
+    }
+    
+    public int getKnightCount() {
+        return knights;
+    }
+}
