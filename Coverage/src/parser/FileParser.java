62a0053c030f405ea26718ceeace37ae09f4fd32
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-05-20 10:19:01 +0200

    Parse files in FileParser

--- Coverage/src/parser/FileParser.java
+++ Coverage/src/parser/FileParser.java
@@ -1,5 +1,10 @@
 package parser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 import problems.AbstractCoverageProblem;
 
 /**
@@ -7,17 +12,55 @@ import problems.AbstractCoverageProblem;
  * Use to create instances of coverage problems from descriptive files.
  */
 public class FileParser {
+    private final String[] _supportedPieces = {" ", "*"};
+    private int _boardSizeX = 0;
+    private int _boardSizeY = 0;
+    private ArrayList<ArrayList<String>> _boardContent = new ArrayList();
     
     public FileParser(String inputFile) {
+        try {
+            scanFile(inputFile);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Could not find file " + inputFile);
+        }
+    }
+    
+    private void scanFile(String inputFile) throws FileNotFoundException {
+        final Scanner s = new Scanner(new File(inputFile));
+        while(s.hasNextLine()) {
+            final String line = s.nextLine();
+            processLine(line);
+        }
+    }
+    
+    private void processLine(String line) {
+        ArrayList<String> newLine = new ArrayList();
         
+        // first line
+        if (_boardSizeY == 0) {
+            String[] lineContent = line.split(" ");
+            _boardSizeX = lineContent.length;
+            Collections.addAll(newLine, lineContent);
+        }
+        
+        // other lines
+        else {
+            for (int i=0; i<line.length(); i+= 2) {
+                newLine.add(String.valueOf(line.charAt(i)));
+            }
+        }
+        System.out.println(String.join("-", newLine));
+        assert(_boardSizeX == newLine.size());
+        _boardContent.add(newLine);
+        _boardSizeY += 1;
     }
     
     public int getBoardSizeX() {
-        return 1;
+        return _boardSizeX;
     }
     
     public int getBoardSizeY() {
-        return 1;
+        return _boardSizeY;
     }
     
     public void fillProblemPieces(AbstractCoverageProblem problem) {
